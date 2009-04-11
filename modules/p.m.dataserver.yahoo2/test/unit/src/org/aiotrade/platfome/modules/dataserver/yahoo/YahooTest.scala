31041007fe102f9ee44f9c7c2e064790cc3800a8
Caoyuan Deng <dcaoyuan@gmail.com>
2009-04-11 15:07:56 +0800

    Use nbproject/harness ant xml

---
+++ modules/p.m.dataserver.yahoo2/test/unit/src/org/aiotrade/platfome/modules/dataserver/yahoo/YahooTest.scala
@@ -0,0 +1,31 @@
+/*
+ * Test.scala
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.aiotrade.platfome.modules.dataserver.yahoo
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert._
+import org.aiotrade.lib.math.timeseries.Frequency
+
+class YahooTest {
+
+    @Before
+    def setUp(): Unit = {
+    }
+
+    @After
+    def tearDown(): Unit = {
+    }
+
+    @Test
+    def example() = {
+        println(Frequency.DAILY)
+    }
+
+}
