64794386fab4a4479b65f4ea0c1cf9ba4c30baa5
Caoyuan Deng <dcaoyuan@gmail.com>
2009-11-11 12:27:04 +0800

    Fixed bug in VolumeChart

--- modules/lib.charting/src/org/aiotrade/lib/charting/chart/VolumeChart.scala
+++ modules/lib.charting/src/org/aiotrade/lib/charting/chart/VolumeChart.scala
@@ -76,7 +76,7 @@ class VolumeChart extends AbstractChart {
       var close  = Null.Float
       var high   = -Math.MAX_FLOAT
       var low    = +Math.MAX_FLOAT
-      var volume = Null.Float
+      var volume = -Math.MAX_FLOAT // we are going to get max of volume during nBarsCompressed
       var i = 0
       while (i < nBarsCompressed) {
         val time = tb(bar + i)
@@ -95,7 +95,7 @@ class VolumeChart extends AbstractChart {
         i += 1
       }
 
-      if (Null.not(volume) /* means we've got volume value */) {
+      if (volume >= 0 /* means we've got volume value */) {
         val color = if (close >= open) LookFeel().getPositiveColor else LookFeel().getNegativeColor
         setForeground(color)
                 
