e3ccd2135bdac4d4d70b7bddb02de4eca7df33e9
Caoyuan Deng <dcaoyuan@gmail.com>
2010-02-04 02:59:03 +0800

    Upgrated to amqp-client 1.6.0

--- libs/lib.amqp/src/main/scala/org/aiotrade/lib/amqp/AMQPSender.scala
+++ libs/lib.amqp/src/main/scala/org/aiotrade/lib/amqp/AMQPSender.scala
@@ -1,24 +1,14 @@
 package org.aiotrade.lib.amqp
 
 import com.rabbitmq.client.AMQP
-import com.rabbitmq.client.Channel
-import com.rabbitmq.client.Connection
 import com.rabbitmq.client.ConnectionFactory
 import java.io.ByteArrayOutputStream
 import java.io.ObjectOutputStream
 
-abstract class AMQPSender[T](cf: ConnectionFactory, host: String, port: Int, exchange: String){
+class AMQPSender[T](cf: ConnectionFactory, host: String, port: Int, exchange: String){
 
-  val (conn, channel, ticket) = connect
-
-  private def connect: (Connection, Channel, Int) = {
-    val conn = cf.newConnection(host, port)
-    val channel = conn.createChannel
-    val ticket = configure(channel)
-    (conn, channel, ticket)
-  }
-
-  def configure(channel: Channel): Int
+  val conn = cf.newConnection(host, port)
+  val channel = conn.createChannel
 
   def send(msg: T, routingKey: String, props: AMQP.BasicProperties) {
     val bytes = new ByteArrayOutputStream
@@ -28,7 +18,7 @@ abstract class AMQPSender[T](cf: ConnectionFactory, host: String, port: Int, exc
 
     val body = bytes.toByteArray
 
-    channel.basicPublish(ticket, exchange, routingKey, props, body)
+    channel.basicPublish(exchange, routingKey, props, body)
     //println(msg + " sent: routingKey=" + routingKey + " size=" + body.length)
   }
 }
