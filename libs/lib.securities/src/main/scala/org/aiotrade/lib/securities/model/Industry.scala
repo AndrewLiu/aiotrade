0110904e9bf43c16e243738668f845704f3a4877
Caoyuan Deng <dcaoyuan@gmail.com>
2010-05-29 14:44:07 +0800

    Copied scala.swing.Publisher, Reactor etc, will try to get them scala.actors.reactor.

--- libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/model/Industry.scala
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/model/Industry.scala
@@ -3,15 +3,17 @@ package org.aiotrade.lib.securities.model
 import ru.circumflex.orm.Table
 
 object Industries extends Table[Industry] {
-  val category = "category" VARCHAR(30) DEFAULT("''") //field(Industry.category)
-  val code = "code" VARCHAR(10) DEFAULT("''") //field(Industry.code)
-  val name = "name" VARCHAR(30) DEFAULT("''") //field(Industry.name)
+  val category = "category" VARCHAR(30) DEFAULT("''")
+  val level = "level" INTEGER
+  val code = "code" VARCHAR(10) DEFAULT("''")
+  val name = "name" VARCHAR(30) DEFAULT("''")
 
-  def companies = inverse(CompanyIndustries.industry) // oneToMany(CompanyIndustry.industry)
+  def companies = inverse(CompanyIndustries.industry)
 }
 
 class Industry {
   var category: String = ""
+  var level: Int = 0
   var code: String = ""
   var name: String = ""
 
---
+++ libs/lib.util/src/main/scala/org/aiotrade/lib/util/actors/Event.scala
@@ -0,0 +1,3 @@
+package org.aiotrade.lib.util.actors
+
+trait Event
