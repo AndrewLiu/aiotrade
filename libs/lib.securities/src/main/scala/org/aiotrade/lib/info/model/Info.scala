7f5b14a30af2fb74b1af94bf4565c216edc45816
Guibin Zhang <guibin.beijing@gmail.com>
2010-08-22 12:25:21 +0800

    Make infos private

--- libs/lib.securities/src/main/scala/org/aiotrade/lib/info/model/Info.scala
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/info/model/Info.scala
@@ -12,19 +12,21 @@ abstract class Infos extends Table[Info] {
 }
 
 class Info extends TVal with Flag {
-  var infos: List[InfoContent] = List()
+  private var _infos: List[InfoContent] = List()
+
+  def infos = _infos
 
   def += [T <: InfoContent](value: T){
     assert(value.publishTime == this.time,
              value + " is appended to a different TVal with time=" + this.time)
 
-    infos = infos :+ value
+    _infos = _infos :+ value
   }
 
   def ++= [T <: InfoContent](values: List[T]){
     values.foreach(v => assert(v.publishTime == this.time,
              v + " is appended to a different TVal with time=" + this.time))
 
-    infos = infos ++ values
+    _infos = _infos ++ values
   }
 }
