7f7a8b1b3ecdb21e5fb7ee542401d3d133aee741
Caoyuan Deng <dcaoyuan@gmail.com>
2010-08-27 11:29:56 +0800

    Color of SignalTopComponent

--- modules/modules.ui/src/main/scala/org/aiotrade/modules/ui/netbeans/windows/SignalTopComponent.scala
+++ modules/modules.ui/src/main/scala/org/aiotrade/modules/ui/netbeans/windows/SignalTopComponent.scala
@@ -1,6 +1,7 @@
 package org.aiotrade.modules.ui.netbeans.windows
 
 import java.awt.BorderLayout
+import java.awt.Color
 import java.awt.Component
 import java.awt.Font
 import java.text.SimpleDateFormat
@@ -15,8 +16,10 @@ import javax.swing.SwingUtilities
 import javax.swing.event.ListSelectionEvent
 import javax.swing.event.ListSelectionListener
 import javax.swing.table.AbstractTableModel
+import javax.swing.table.DefaultTableCellRenderer
 import javax.swing.table.TableCellRenderer
 import org.aiotrade.lib.collection.ArrayList
+import org.aiotrade.lib.charting.laf.LookFeel
 import org.aiotrade.lib.math.signal.Sign
 import org.aiotrade.lib.math.signal.Signal
 import org.aiotrade.lib.math.signal.SubSignalEvent
@@ -62,10 +65,14 @@ class SignalTopComponent extends TopComponent with Reactor {
           case 0 =>
             val sec = Exchange.secOf(event.uniSymbol).getOrElse(return null)
             val timeZone = sec.exchange.timeZone
-            val cal = Calendar.getInstance(timeZone)
-            val df = new SimpleDateFormat("HH:mm:ss")
+            val df = event.freq match {
+              case "1D" => new SimpleDateFormat("MM-dd")
+              case "1m" => new SimpleDateFormat("HH:mm")
+              case _ => new SimpleDateFormat("MM-dd")
+            }
             df.setTimeZone(timeZone)
 
+            val cal = Calendar.getInstance(timeZone)
             cal.setTimeInMillis(event.signal.time)
             df format cal.getTime
           case 1 => event.uniSymbol
@@ -84,19 +91,25 @@ class SignalTopComponent extends TopComponent with Reactor {
     }
 
     signalTable = new JTable(signalModel)
-    //executionTable.setDefaultRenderer(classOf[Object], new TrendSensitiveCellRenderer)
+    signalTable.setDefaultRenderer(classOf[Object], new TrendSensitiveCellRenderer)
     signalTable.setFocusable(false)
     signalTable.setCellSelectionEnabled(false)
     signalTable.setShowHorizontalLines(false)
     signalTable.setShowVerticalLines(false)
-    //signalTable.setForeground(Color.BLACK)
-    //signalTable.setBackground(Color.WHITE)
+    signalTable.setForeground(Color.WHITE)
+    signalTable.setBackground(LookFeel().backgroundColor)
     signalTable.setFillsViewportHeight(true)
     signalTable.getTableHeader.setDefaultRenderer(new TableHeaderRenderer)
 
+    // --- set column width
+    var columnModel = signalTable.getColumnModel
+    columnModel.getColumn(0).setPreferredWidth(20)
+    columnModel.getColumn(1).setPreferredWidth(35)
+    columnModel.getColumn(2).setPreferredWidth(20)
+
     val scrollPane = new JScrollPane
     scrollPane.setViewportView(signalTable)
-    //scrollPane.setBackground(Color.WHITE)
+    scrollPane.setBackground(LookFeel().backgroundColor)
     scrollPane.setFocusable(true)
 
     setLayout(new BorderLayout)
@@ -156,8 +169,6 @@ class SignalTopComponent extends TopComponent with Reactor {
   private def showCell(table: JTable, row: Int, column: Int) {
     val rect = table.getCellRect(row, column, true)
     table.scrollRectToVisible(rect)
-    table.clearSelection
-    table.setRowSelectionInterval(row, row)
   }
 
   override def getPersistenceType: Int = {
@@ -171,12 +182,13 @@ class SignalTopComponent extends TopComponent with Reactor {
 
   class TableHeaderRenderer extends JLabel with TableCellRenderer {
     private val defaultFont = new Font("Dialog", Font.BOLD, 12)
+
     setOpaque(true)
     def getTableCellRendererComponent(table: JTable, value: Object, isSelected: Boolean,
                                       hasFocus: Boolean, row: Int, col: Int): Component = {
 
-      //setForeground(Color.BLACK)
-      //setBackground(Color.WHITE)
+      setForeground(Color.WHITE)
+      setBackground(LookFeel().backgroundColor)
       setFont(defaultFont)
       setHorizontalAlignment(SwingConstants.CENTER)
       setText(value.toString)
@@ -191,6 +203,53 @@ class SignalTopComponent extends TopComponent with Reactor {
     override def revalidate() {}
     override protected def firePropertyChange(propertyName: String, oldValue: Object, newValue: Object) {}
     override def firePropertyChange(propertyName: String, oldValue: Boolean, newValue: Boolean) {}
+  }
+
+
+  class TrendSensitiveCellRenderer extends DefaultTableCellRenderer {
+    private val defaultFont = new Font("Dialog", Font.PLAIN, 12)
+    private val bgColorSelected = new Color(56, 86, 111)
+
+    setForeground(Color.WHITE)
+    setBackground(LookFeel().backgroundColor)
+    setOpaque(true)
+
+    override def getTableCellRendererComponent(table: JTable, value: Object, isSelected: Boolean,
+                                               hasFocus: Boolean, row: Int, column: Int): Component = {
+
+      /** Beacuse this will be a sinleton for all cells, so, should clear it first */
+      setFont(defaultFont)
+      setForeground(Color.WHITE)
+      if (isSelected) {
+        setBackground(bgColorSelected)
+      } else {
+        setBackground(LookFeel().backgroundColor)
+      }
+      setText(null)
+
+      if (value != null) {
+        column match {
+          case 0 => // Time
+            setHorizontalAlignment(SwingConstants.LEADING)
+          case 1 => // Symbol
+            setHorizontalAlignment(SwingConstants.TRAILING)
+          case 2 => // Sign
+            if (row - 1 >= 0) {
+              value.toString.trim match {
+                case "买" => setForeground(LookFeel().getPositiveBgColor)
+                case "卖" => setForeground(LookFeel().getNegativeBgColor)
+                case _    => setForeground(LookFeel().getNegativeBgColor)
+              }
+            }
+          case 3 => // Name
+            setHorizontalAlignment(SwingConstants.LEADING)
+        }
+
+        setText(value.toString)
+      }
+
+      this
+    }
   }
 
 }
