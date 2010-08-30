0804bfdfd411f10dd5572ca9528af544a008b9aa
Guibin Zhang <guibin.beijing@gmail.com>
2010-08-30 14:32:50 +0800

    Sector implements the InfoContent

--- libs/lib.securities/src/main/scala/org/aiotrade/lib/sector/model/Portfolio.scala
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/sector/model/Portfolio.scala
@@ -1,9 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package org.aiotrade.lib.sector.model
+
 import ru.circumflex.orm.Table
 
 object  Portfolios extends Table[Portfolio]{
--- libs/lib.securities/src/main/scala/org/aiotrade/lib/sector/model/Sector.scala
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/sector/model/Sector.scala
@@ -1,7 +1,13 @@
 package org.aiotrade.lib.sector.model
 
 import ru.circumflex.orm._
+import scala.collection.mutable.Buffer
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.Map
+import org.aiotrade.lib.securities.model.Flag
+import org.aiotrade.lib.info.model.InfoContent
+import org.aiotrade.lib.math.timeseries.TVal
+import scala.collection.JavaConversions._
 
 object Sectors extends Table[Sector] {
   val name = "name" VARCHAR(30)
@@ -29,9 +35,47 @@ object Sectors extends Table[Sector] {
   def portfolios = inverse(Portfolios.sector)
 }
 
-class Sector {
+class Sector extends TVal with Flag with InfoContent{
   var name : String = ""
   var code : String = ""
   var portfolio : Portfolio = _
   var portfolios : List[Portfolio] = Nil
+
+  def publishTime: Long = this.time
+  def weight: Float = 0F
+  def link: String = ""
+
+  def exportToMap: Map[String, String] = Map("" -> "")
+
+  def exportToJavaMap: java.util.Map[String, String] = exportToMap
+
+  def exportToList: Buffer[Map[String, String]] = {
+
+    val list = Buffer[Map[String, String]]()
+
+    if(portfolio != null && portfolio.breakouts != null){
+      for(portfolio <- portfolio.breakouts){
+        val map = Map[String, String]("SECURITY_CODE" -> portfolio.sec.uniSymbol)
+        map += ("SECURITY_NAME" -> portfolio.sec.secInfo.name)
+        map += ("ENTER_TIME" -> this.time.toString)
+        list += map
+      }
+    }
+    list
+  }
+
+  def exportToJavaList: java.util.List[java.util.Map[String, String]] = {
+    
+    val list = new java.util.ArrayList[java.util.Map[String, String]]
+    
+    if(portfolio != null && portfolio.breakouts != null){
+      for(portfolio <- portfolio.breakouts){
+        val map = Map[String, String]("SECURITY_CODE" -> portfolio.sec.uniSymbol)
+        map += ("SECURITY_NAME" -> portfolio.sec.secInfo.name)
+        map += ("ENTER_TIME" -> this.time.toString)
+        list.add(map)
+      }
+    }
+    list
+  }
 }
