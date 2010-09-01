a202f75a9556448e61ada5c71a5ab622081fa1b1
Caoyuan Deng <dcaoyuan@gmail.com>
2010-09-02 01:00:20 +0800

    Fixed Crypto

--- libs/lib.util/src/main/scala/org/aiotrade/lib/util/security/Crypto.scala
+++ libs/lib.util/src/main/scala/org/aiotrade/lib/util/security/Crypto.scala
@@ -2,15 +2,30 @@ package org.aiotrade.lib.util.security
 
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-import java.io.InputStream
 import java.io.ObjectInputStream
 import java.io.ObjectOutputStream
+import java.security.SecureRandom
 import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 object Crypto {
 
-  def keyToByteArray(key: SecretKey): Array[Byte] = {
+  private val AES = "AES"
+
+  // KeyGenerator objects are reusable
+  private val keygen = KeyGenerator.getInstance(AES)
+  private val random = new SecureRandom
+
+  def generateKey: SecretKey = {
+    keygen.init(random)
+    val key = keygen.generateKey
+    val raw = key.getEncoded
+    new SecretKeySpec(raw, AES)
+  }
+
+  def keyToBytes(key: SecretKey): Array[Byte] = {
     val os = new ByteArrayOutputStream
     val out = new ObjectOutputStream(os)
     out.writeObject(key)
@@ -18,63 +33,35 @@ object Crypto {
     os.toByteArray
   }
 
-  def keyFromByteArray(bytes: Array[Byte]): SecretKey = {
+  def keyFromBytes(bytes: Array[Byte]): SecretKey = {
     val in = new ObjectInputStream(new ByteArrayInputStream(bytes))
     in.readObject.asInstanceOf[SecretKey]
   }
 
   def encrypt(content: String, key: SecretKey): Array[Byte] = {
-    encrypt(content.getBytes("UTF-8"), key: SecretKey)
+    encrypt(content.getBytes("UTF-8"), key)
   }
 
   def encrypt(bytes: Array[Byte], key: SecretKey): Array[Byte] = {
-    val mode = Cipher.ENCRYPT_MODE
-    val cipher = Cipher.getInstance("AES")
-    cipher.init(mode, key)
-    crypt(bytes, cipher)
+    val cipher = Cipher.getInstance(AES)
+    cipher.init(Cipher.ENCRYPT_MODE, key)
+    cipher.doFinal(bytes)
   }
 
   def decrypt(bytes: Array[Byte], key: SecretKey): Array[Byte] = {
-    val mode = Cipher.ENCRYPT_MODE
-    val cipher = Cipher.getInstance("AES")
-    cipher.init(mode, key)
-    crypt(bytes, cipher)
+    val cipher = Cipher.getInstance(AES)
+    cipher.init(Cipher.DECRYPT_MODE, key)
+    cipher.doFinal(bytes)
   }
 
-  private def crypt(bytes: Array[Byte], cipher: Cipher): Array[Byte] = {
-    val is = new ByteArrayInputStream(bytes)
-    val result = crypt(is, cipher)
-    is.close
-    result
+  // --- simple test
+  def main(args: Array[String]) {
+    keygen.init(random)
+    val keySpec = generateKey
+    val en = encrypt("test", keySpec)
+    println(en.mkString("(", ",", ")"))
+    val de = decrypt(en, keySpec)
+    println(new String(de, "UTF-8"))
   }
 
-  private def crypt(is: InputStream, cipher: Cipher): Array[Byte] = {
-    val blockSize = cipher.getBlockSize
-    val outputSize = cipher.getOutputSize(blockSize)
-    val inBytes = new Array[Byte](blockSize)
-    var outBytes = new Array[Byte](outputSize)
-
-    val os = new ByteArrayOutputStream
-    var inLength = 0
-    var more = true
-    while (more) {
-      val inLength = is.read(inBytes)
-      if (inLength == blockSize) {
-        val outLength = cipher.update(inBytes, 0, blockSize, outBytes)
-        os.write(outBytes, 0, outLength)
-      } else {
-        more = false
-      }
-    }
-
-    if (inLength > 0) {
-      outBytes = cipher.doFinal(inBytes, 0, inLength)
-    } else {
-      outBytes = cipher.doFinal
-    }
-    os.write(outBytes)
-
-    os.close
-    os.toByteArray
-  }
 }
