97fd1389eacd176e7727d9ba5b380f2b37000a61
Guibin Zhang <guibin.beijing@gmail.com>
2010-09-01 10:14:47 +0800

    Add export for QuoteInfoSnapshot

--- libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/dataserver/QuoteInfoDataServer.scala
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/dataserver/QuoteInfoDataServer.scala
@@ -10,6 +10,7 @@ import org.aiotrade.lib.info.model.GeneralInfo
 import org.aiotrade.lib.info.model.ContentCategory
 import org.aiotrade.lib.math.timeseries.datasource.DataServer
 import org.aiotrade.lib.securities.model.Sec
+import scala.collection.mutable.HashMap
 import scala.collection.mutable.ListBuffer
 import java.util.logging.Logger
 import org.aiotrade.lib.math.timeseries.TSerEvent
@@ -27,10 +28,24 @@ class QuoteInfo extends TVal {
 }
 
 case class QuoteInfoSnapshot(publishTime : Long, title: String, url : String,
-                             combinValue : Long, content : String, summery : String,
-                             category : List[ContentCategory], secs : List[Sec] ) extends Event
+                             combinValue : Long, content : String, summary : String,
+                             category : List[ContentCategory], secs : List[Sec] ) extends Event{
 
-case class QuoteInfoSnapshots(events : List[QuoteInfoSnapshot]) extends Event
+  def export: HashMap[String, Any]= {
+    HashMap[String, Any] ("publishTime" -> publishTime,
+                          "title" -> title,
+                          "url" -> url,
+                          "combinValue" -> combinValue,
+                          "content" -> content,
+                          "summary" -> summary,
+                          "category" -> {for(cate <- category) yield cate.code},
+                          "symbol" -> {for(sec <- secs) yield sec.uniSymbol})
+  }
+}
+
+case class QuoteInfoSnapshots(events : List[QuoteInfoSnapshot]) extends Event {
+  def export: List[HashMap[String, Any]] = for(event <- events) yield event.export
+}
 
 object QuoteInfoDataServer extends Publisher
 
@@ -66,9 +81,9 @@ abstract class QuoteInfoDataServer extends  DataServer[QuoteInfo] {
         case None => null
       }
       val quoteInfo = QuoteInfoSnapshot(info.generalInfo.publishTime, info.generalInfo.title,
-                                    info.generalInfo.url, info.generalInfo.combinValue,
-                                    info.content, info.summery,info.categories.toList,
-                                    info.secs.toList)
+                                        info.generalInfo.url, info.generalInfo.combinValue,
+                                        info.content, info.summery,info.categories.toList,
+                                        info.secs.toList)
       allQuoteInfo += quoteInfo
     }
 
