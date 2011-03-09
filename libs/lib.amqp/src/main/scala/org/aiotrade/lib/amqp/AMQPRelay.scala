b73fd9a89ce7e1241b6b18304cffd8ff04830458
Guibin Zhang <guibin.beijing@gmail.com>
2011-03-09 18:03:03 +0800

    Fix null pointer exception in AMQPRelay

--- libs/lib.amqp/src/main/scala/org/aiotrade/lib/amqp/AMQPRelay.scala
+++ libs/lib.amqp/src/main/scala/org/aiotrade/lib/amqp/AMQPRelay.scala
@@ -29,35 +29,40 @@ object AMQPRelay {
     val masterUsername = config.getString("amqp.relay.master.username", "guest")
     val masterPassword = config.getString("amqp.relay.master.password", "guest")
     val masterExchange = config.getString("amqp.relay.master.exchange", "market.internal")
-    val masterQueueKey = config.getString("amqp.relay.master.queuekey", "source.file.cndbf")
-    val masterPrefetchCount = config.getInt("amqp.relay.master.prefetchCount", 0)
+    val masterQueue = config.getString("amqp.relay.master.queue", "file.cndbf.master")
+    val masterKey = config.getString("amqp.relay.master.routingkey", "source.file.cndbf")
+    val masterPrefetchCount = config.getInt("amqp.relay.master.prefetchcount", 0)
 
     val slaveHost = config.getString("amqp.relay.slave.host", "localhost")
     val slavePort = config.getInt("amqp.relay.slave.port", 5673)
     val slaveUsername = config.getString("amqp.relay.slave.username", "guest")
     val slavePassword = config.getString("amqp.relay.slave.password", "guest")
     val slaveExchange = config.getString("amqp.relay.slave.exchange", "market.internal")
-    val slaveQueueKey = config.getString("amqp.relay.slave.queuekey", "source.file.cndbf")
+    val slaveQueue = config.getString("amqp.relay.slave.queue", "file.cndbf.master")
+    val slaveKey = config.getString("amqp.relay.slave.routingkey", "source.file.cndbf")
     val slavePrefetchCount = config.getInt("amqp.relay.slave.prefetchCount", 0)
 
     val masterFactory = new ConnectionFactory
     masterFactory.setHost(masterHost)
     masterFactory.setPort(masterPort)
-    masterFactory.setUsername("guest")
-    masterFactory.setPassword("guest")
+    masterFactory.setUsername(masterUsername)
+    masterFactory.setPassword(masterPassword)
     masterFactory.setVirtualHost("/")
     masterFactory.setRequestedHeartbeat(0)
 
     val slaveFactory = new ConnectionFactory
     slaveFactory.setHost(slaveHost)
     slaveFactory.setPort(slavePort)
-    slaveFactory.setUsername("guest")
-    slaveFactory.setPassword("guest")
+    slaveFactory.setUsername(slaveUsername)
+    slaveFactory.setPassword(slavePassword)
     slaveFactory.setVirtualHost("/")
     slaveFactory.setRequestedHeartbeat(0)
 
-    consumer = new RelayConsumer(masterFactory, masterExchange, masterQueueKey, masterPrefetchCount)
-    publisher = new RelayPublisher(slaveFactory, slaveExchange, slaveQueueKey, slavePrefetchCount)
+    consumer = new RelayConsumer(masterFactory, masterExchange, masterQueue, masterKey, masterPrefetchCount)
+    publisher = new RelayPublisher(slaveFactory, slaveExchange, slaveQueue, slaveKey, slavePrefetchCount)
+
+    consumer.listenTo(publisher)
+    publisher.listenTo(consumer)
 
     //Be sure the safty exiting.
     Runtime.getRuntime.addShutdownHook(new Thread(){
@@ -71,7 +76,7 @@ object AMQPRelay {
   }
 
   def start = {
-    if(!isInitialized) {
+    if(isInitialized) {
       consumer.connect
       publisher.connect
     } else {
@@ -91,23 +96,23 @@ object AMQPRelay {
 }
 
 import AMQPRelay._
-class RelayConsumer(factory: ConnectionFactory, exchange: String, queueKey: String, prefetchCount: Int = 0) extends AMQPDispatcher(factory, exchange) {
+class RelayConsumer(factory: ConnectionFactory, exchange: String, queue: String, bindingKey: String, prefetchCount: Int = 0) extends AMQPDispatcher(factory, exchange) {
 
   override def configure(channel: Channel): Option[Consumer] = {
     
     channel.basicQos(prefetchCount)
     channel.exchangeDeclare(exchange, "direct")
-    channel.queueDeclare(queueKey, true, false, false, null)
-    channel.queueBind(queueKey, exchange, queueKey)
+    channel.queueDeclare(queue, true, false, false, null)
+    channel.queueBind(queue, exchange, bindingKey)
 
-    log.info("Declared master exchange: " + exchange + ", master queueKey: " + queueKey)
+    log.info("Declared master_exchange: " + exchange + ", master_queue: " + queue + ", master_key: " + bindingKey)
 
     //We want isAutoAck = false, after relaying the message, it will be acknowledged explicitly.
     val consumer =  new AMQPConsumer(channel, false) {
       override def handleAck(isAutoAck: Boolean, channel: Channel, envelope: Envelope) {}
     }
     
-    channel.basicConsume(queueKey, consumer.isAutoAck, consumer)
+    channel.basicConsume(queue, consumer.isAutoAck, consumer)
     
     Some(consumer)
   }
@@ -123,13 +128,13 @@ class RelayConsumer(factory: ConnectionFactory, exchange: String, queueKey: Stri
   reactions += {
     case AMQPAcknowledge(deliveryTag) =>
       channel.foreach{ch => ch.basicAck(deliveryTag, false)}
+      log.fine(deliveryTag + " acknowledged")
   }
-  listenTo(AMQPRelay.publisher)
-  
+
 }
 
 
-class RelayPublisher(factory: ConnectionFactory, exchange: String, queueKey: String, prefetchCount: Int = 0) extends AMQPDispatcher(factory, exchange) {
+class RelayPublisher(factory: ConnectionFactory, exchange: String, queue: String, bindingKey: String, prefetchCount: Int = 0) extends AMQPDispatcher(factory, exchange) {
 
   private val log = Logger.getLogger(getClass.getName)
 
@@ -138,10 +143,10 @@ class RelayPublisher(factory: ConnectionFactory, exchange: String, queueKey: Str
 
     channel.basicQos(prefetchCount)
     channel.exchangeDeclare(exchange, "direct")
-    channel.queueDeclare(queueKey, true, false, false, null)
-    channel.queueBind(queueKey, exchange, queueKey)
+    channel.queueDeclare(queue, true, false, false, null)
+    channel.queueBind(queue, exchange, bindingKey)
 
-    log.info("Declared slave exchange: " + exchange + ", master queueKey: " + queueKey)
+    log.info("Declared slave_exchange: " + exchange + ", slave_queue: " + queue + ", slave_key: " + bindingKey)
     
     None
   }
@@ -149,10 +154,10 @@ class RelayPublisher(factory: ConnectionFactory, exchange: String, queueKey: Str
   reactions += {
     case msg: AMQPMessage =>
       //Publish the msg to the slave AMQP
-      publish(exchange, queueKey, msg.props, msg.body)
+      publish(exchange, bindingKey, msg.props, msg.body)
       //Then acknowledge the delivery to master AMQP
+      log.fine(msg.envelope.getDeliveryTag + " relayed")
       publish(AMQPAcknowledge(msg.envelope.getDeliveryTag))
   }
-  listenTo(AMQPRelay.consumer)
-  
+
 }
