4f4cb47fad29e4f89845150bd9ab0b2aab5c7bf9
Caoyuan Deng <dcaoyuan@gmail.com>
2011-05-24 14:44:51 +0800

    Refined Evt.scala

--- libs/lib.util/src/main/scala/org/aiotrade/lib/util/actors/Evt.scala
+++ libs/lib.util/src/main/scala/org/aiotrade/lib/util/actors/Evt.scala
@@ -30,6 +30,7 @@
  */
 package org.aiotrade.lib.util.actors
 
+import org.aiotrade.lib.util.ClassHelper
 import scala.collection.mutable
 
 /**
@@ -39,23 +40,32 @@ import scala.collection.mutable
  * 'T' is the type of evt value
  * '(Int, T)' is the type of each evt message (EvtMessage)
  * 
- * Instead of using case class as evt message, the design here uses a plain
- * Tuple as the evt message, so:
+ * Instead of using case class as evt message, the design here uses a plain Tuple
+ * as the evt message, so:
  * 1. It's easier to keep off from possible serialization issue for lots concrete classes
  * 2. The meta data such as 'doc', 'tpeClass' are stored in evt definition for
  *    each type of evt. Only the message data is stored in each evt message
  * 3. The serialization size of evt message is smaller.
+ * 4. We can pattern match it via named extract, or via regural Tuple match 
  * 
- * @param [T] the type of the evt value
+ * @param [T] the type of the evt value. 
+ *        For list, although we support collection.Seq[_] type of T, but it's better 
+ *        to use JVM type safed Array[_], since we here have to check all elements 
+ *        of value to make sure the pattern match won't be cheated. 
+ *        For varargs, use type safed Tuple instead of List.
+ *        @see unapply
  * @param tag an unique id in int for this type of Evt
  * @param doc the document of this Evt
  * 
  * @author Caoyuan Deng
  */
-abstract class Evt[T <: AnyRef](val tag: Int, val doc: String = "")(implicit m: Manifest[T]) {
-  type tpe = T
-  type EvtMessage = (Int, T)
-  val tpeClass = m.erasure
+abstract class Evt[T: Manifest](val tag: Int, val doc: String = "") {
+  type Type = T
+  type EvtMsg = (Int, T)
+  
+  private val m = manifest[T]
+  private val typeArguments = m.typeArguments map (_.erasure)
+  val typeClass = m.erasure
   
   assert(!Evt.tagToEvt.contains(tag), "Tag: " + tag + " already existed!")
   Evt.tagToEvt(tag) = this
@@ -70,71 +80,119 @@ abstract class Evt[T <: AnyRef](val tag: Int, val doc: String = "")(implicit m: 
    * Return the evt message that is to be passed to. the evt message is wrapped in
    * a tuple in form of (tag, evtValue)
    */
-  def apply(evtValue: T): (Int, T) = (tag, evtValue)
+  def apply(evtVal: T): (Int, T) = (tag, evtVal)
   
   /** 
-   * @Note Since T is erasued after compiled, should check type of evt message via isInstance.
-   * And, don't write as unapply(evt: (Int, T)), which will confuse the compiler to generate
+   * @Note Since T is erasued after compiled, should check type of evt message via Manifest.
+   * And, don't write as unapply(evtMsg: (Int, T)), which will confuse the compiler to generate
    * wrong code for match {case .. case ..}
    */
-  def unapply(evtMessage: AnyRef): Option[T] = evtMessage match {
-    case (`tag`, value: T) if tpeClass.isInstance(value) => Some(value)
+  def unapply(evtMsg: Any): Option[T] = evtMsg match {
+    case (`tag`, value: T) if ClassHelper.isInstance(typeClass, value) =>
+      // we will do 1-level type arguments check, and won't deep check t's type parameter anymore
+      value match {
+        case x: collection.Seq[_] =>
+          val t = typeArguments.head
+          val vs = x.iterator
+          while (vs.hasNext) {
+            if (!ClassHelper.isInstance(t, vs.next)) 
+              return None
+          }
+          Some(value)
+        case x: Product if (ClassHelper.isTuple(x))=>
+          val vs = x.productIterator
+          val ts = typeArguments.iterator
+          while (vs.hasNext) {
+            if (!ClassHelper.isInstance(ts.next, vs.next)) 
+              return None
+          }
+          Some(value)
+        case _ => Some(value)
+      }
     case _ => None
   }
   
   override def toString = {
-    "Evt(tag=" + tag + ", tpe=" + m.erasure + ", doc=\"" + doc + "\")"
+    "Evt(tag=" + tag + ", tpe=" + typeClass + ", doc=\"" + doc + "\")"
   }
 }
 
 object Evt {
   private val tagToEvt = new mutable.HashMap[Int, Evt[_]]
   
-  def evtTpeOf(tag: Int): Option[Class[_]] = tagToEvt.get(tag) map {_.tpeClass}
+  def typeOf(tag: Int): Option[Class[_]] = tagToEvt.get(tag) map {_.typeClass}
   def schemaOf(tag: Int): Option[String] = tagToEvt.get(tag) map {_.schema}
-  
+  def allEvts = tagToEvt.valuesIterator
+    
   // -- simple test
   def main(args: Array[String]) {
     object StrEvt extends Evt[String](-1)
-    object IntEvt extends Evt[java.lang.Integer](-2)
-    object SeqEvt extends Evt[(Int, String)](-3, "value, name")
+    object IntEvt extends Evt[Int](-2)
+    object ArrEvt extends Evt[Array[String]](-3)
+    object LstEvt extends Evt[List[String]](-4)
+    object MulEvt extends Evt[(Int, String, Double)](-5, "id, name, value")
     
     println("\n==== apis: ")
     println(StrEvt)
     println(IntEvt)
-    println(SeqEvt)
+    println(ArrEvt)
+    println(LstEvt)
+    println(MulEvt)
     
-    val a = StrEvt("a")
-    val b = IntEvt(1)
-    val c = SeqEvt(1, "a")
-    val bada = (-1, 1)
-    val badb = (-2, "a")
+    println(allEvts)
     
-    val evtMsgs = List(a, b, c, bada, badb)
+    val goodEvtMsgs = List(
+      StrEvt("a"),
+      IntEvt(8),
+      ArrEvt(Array("a", "b")),
+      LstEvt(List("a", "b")),
+      MulEvt(8, "a", 8.0),
+      MulEvt(8, "a", 8)
+    )
     
-    println("\n==== evt messages: ")
-    evtMsgs foreach println
+    val badEvtMsgs = List(
+      (-1, 8),
+      (-2, "a"),
+      (-3, Array(8, "a")),
+      (-3, Array(8, 8)),
+      (-4, List(1, "a")),
+      (-5, (8, "a")),
+      (-5, (8, 8, 8))
+    )
     
-    println("\n==== regular matching: ")
-    evtMsgs foreach regularMatch
+    println("\n==== good evt messages: ")
+    goodEvtMsgs map println
+    println("\n==== bad evt messages: ")
+    badEvtMsgs  map println
     
-    println("\n==== advanced matching: ")
-    evtMsgs foreach advancedMatch
+    println("\n==== regular matched: ")
+    assert(!(goodEvtMsgs map regularMatch).contains(false), "Test failed")
+    println("\n==== regular unmatched: ")
+    //assert(!(badEvtMsgs  map regularMatch).contains(true),  "Test failed")
+    
+    println("\n==== advanced matched: ")
+    assert(!(goodEvtMsgs map advancedMatch).contains(false), "Test failed")
+    println("\n==== advanced unmatched: ")
+    assert(!(badEvtMsgs  map advancedMatch).contains(true),  "Test failed") 
     
     /** The regular match on those evts look like: */
     def regularMatch(v: Any) = v match {
-      case (StrEvt.tag, astr: StrEvt.tpe) => println("Matched: " + v + " => " + astr) 
-      case (IntEvt.tag, aint: IntEvt.tpe) => println("Matched: " + v + " => " + aint)
-      case (SeqEvt.tag, aseq: SeqEvt.tpe) => println("Matched: " + v + " => " + aseq)
-      case _ => println("Unmatched: " + v)
+      case (StrEvt.tag, aval: StrEvt.Type) => println("Matched: " + v + " => " + aval); true
+      case (IntEvt.tag, aval: IntEvt.Type) => println("Matched: " + v + " => " + aval); true
+      case (ArrEvt.tag, aval: ArrEvt.Type) => println("Matched: " + v + " => " + aval); true
+      case (LstEvt.tag, aval: LstEvt.Type) => println("Matched: " + v + " => " + aval); true
+      case (MulEvt.tag, aval: MulEvt.Type) => println("Matched: " + v + " => " + aval); true
+      case _ => println("Unmatched: " + v); false
     }
     
     /** But we'd like a more concise approach: */
     def advancedMatch(v: Any) = v match {
-      case StrEvt(astr) => println("Matched: " + v + " => " + astr) 
-      case IntEvt(aint) => println("Matched: " + v + " => " + aint) 
-      case SeqEvt(aint, astr) => println("Matched: " + v + " => (" + aint + ", " + astr + ")") 
-      case _ => println("Unmatched: " + v)
+      case StrEvt(aval) => println("Matched: " + v + " => " + aval); true
+      case IntEvt(aval) => println("Matched: " + v + " => " + aval); true
+      case ArrEvt(aval) => println("Matched: " + v + " => " + aval); true
+      case LstEvt(aval@List(a: String, b: String)) => println("Matched: " + v + " => " + aval); true
+      case MulEvt(aint: Int, astr: String, adou: Double) => println("Matched: " + v + " => (" + aint + ", " + astr + ", " + adou + ")"); true
+      case _ => println("Unmatched: " + v); false
     }
   }
 }
