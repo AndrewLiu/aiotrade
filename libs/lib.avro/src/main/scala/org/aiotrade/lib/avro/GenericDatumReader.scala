c14646be1ffce943056f5dcab21fbcc0e8a4cfe8
Caoyuan Deng <dcaoyuan@gmail.com>
2011-06-21 01:32:35 +0800

    Preformance tuning.

--- libs/lib.avro/src/main/scala/org/aiotrade/lib/avro/GenericDatumReader.scala
+++ libs/lib.avro/src/main/scala/org/aiotrade/lib/avro/GenericDatumReader.scala
@@ -325,13 +325,13 @@ class GenericDatumReader[T] protected (private var actual: Schema, private var e
   protected def newArray[T: Manifest](old: Any, size: Int, schema: Schema, elementClass: Class[T]): Any = {
     import Schema.Type._
     schema.getElementType.getType match {
-      case RECORD | ARRAY | MAP | UNION | FIXED | STRING | BYTES | NULL => new ArrayList[T](size, elementClass)
       case INT =>     new ArrayList[Int](size)
       case ENUM =>    new ArrayList[Int](size)
       case LONG =>    new ArrayList[Long](size)
       case FLOAT =>   new ArrayList[Float](size)
       case DOUBLE =>  new ArrayList[Double](size)
       case BOOLEAN => new ArrayList[Boolean](size)
+      case RECORD | ARRAY | MAP | UNION | FIXED | STRING | BYTES | NULL => new ArrayList[T](size, elementClass)
       case _ => throw new AvroRuntimeException("Unknown type: " + expected)
     }
   }
