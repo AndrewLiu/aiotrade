535545a115b0ffb222705ee3360a786bf9c643fb
Caoyuan Deng <dcaoyuan@gmail.com>
2011-06-27 03:45:08 +0800

    new avro data

--- libs/lib.securities/src/main/resources/migration/migration20110622.sql
+++ libs/lib.securities/src/main/resources/migration/migration20110622.sql
@@ -1,11 +1,28 @@
-# check secs that lacks sec_infos
-select secs.id, unisymbol from secs join sec_infos on secs.id = sec_infos.secs_id where unisymbol is null;
+# check secs that lacks sec_infos, regular reference:
+select a.id, a.secInfos_id from secs as a left join sec_infos as b on a.secInfos_id=b.id where b.id is null;
+# or,
+select a.id, a.secInfos_id from secs as a where a.secInfos_id not in (select b.id from sec_infos as b);
+# Try another side
+select a.id, a.secInfos_id from secs as a left join sec_infos as b on a.id=b.secs_id where b.id is null;
+# or
+select a.id, a.secInfos_id from secs as a where a.id not in (select b.secs_id from sec_infos as b);
+
+# Some of secs has secInfos_id = -1 ?
+select a.secInfos_id from secs as a where a.secInfos_id=-1;
+# Don't delete them, you should try to find or add corresponding sec_infos later
+
+# check sec_infos that lacks secs
+select a.id, a.secs_id, a.unisymbol from sec_infos as a left join secs on a.secs_id=secs.id where secs.id is null;
+# or
+select a.id, a.secs_id, a.unisymbol from sec_infos as a where a.secs_id not in (select secs.id from secs);
+# you may want to delete them (double check here)
+delete from sec_infos where sec_infos.secs_id not in (select secs.id from secs);
 
 # check if all CRC32(uniSymbol) is great than existed max secs_id, if so, we are safe to do the convert:
-select secs_id, CRC32(uniSymbol) from sec_infos where CRC32(uniSymbol) < (select max(secs_id) from sec_infos);
+select secs_id, crc32(upper(uniSymbol)) from sec_infos where crc32(upper(uniSymbol)) < (select max(secs_id) from sec_infos);
 
 # create table secoldnew
-create table secoldnew select secs_id as oldid, crc32(unisymbol) as newid, unisymbol as crckey from sec_infos;
+create table secoldnew select secs_id as oldid, crc32(upper(unisymbol)) as newid, unisymbol as crckey from sec_infos;
 create index secoldid_idx on secoldnew(oldid);
 create index secnewid_idx on secoldnew(newid);
 
@@ -38,7 +55,7 @@ update companies as a, secoldnew set a.secs_id=secoldnew.newid where a.secs_id=s
 # === sectors id changed
 
 # create table sectoroldnew
-create table sectoroldnew select id as oldid, crc32(concat(category, '.', code)) as newid, concat(category, '.', code) as crckey from sectors;
+create table sectoroldnew select id as oldid, crc32(upper(concat(category, '.', code))) as newid, concat(category, '.', code) as crckey from sectors;
 # update sectors.id
 update sectors as a, sectoroldnew set a.id=sectoroldnew.newid, a.crckey=sectoroldnew.crckey where a.id=sectoroldnew.oldid;
 # update tables that has sectors_id
@@ -47,7 +64,7 @@ update sector_secs as a, sectoroldnew set a.sectors_id=sectoroldnew.newid where 
 # === exchanges id changed
 
 # create table exchangeoldnew
-create table exchangeoldnew select id as oldid, crc32(code) as newid, code as crckey from exchanges;
+create table exchangeoldnew select id as oldid, crc32(upper(code)) as newid, code as crckey from exchanges;
 # update exchanges.id
 update exchanges as a, exchangeoldnew set a.id=exchangeoldnew.newid, a.crckey=exchangeoldnew.crckey where a.id=exchangeoldnew.oldid;
 # update tables that has sectors_id
@@ -69,3 +86,11 @@ update quotes1m as a, secoldnew set a.secs_id=secoldnew.newid where a.secs_id=se
 # Query OK, 181812747 rows affected (12 hours 3 min 28.82 sec)
 # Rows matched: 181812747  Changed: 181812747  Warnings: 0
 
+# try to clean secs and sec_infos
+# Is there sec_infos that has empty unisymbol?
+select * from sec_infos where unisymbol="" or unisymbol is null;
+# Then the corresponding secs has crckey?
+select a.id, secs.id, secs.crckey from sec_infos as a left join secs on a.secs_id=secs.id where unisymbol="" or unisymbol is null;
+
+select a.secInfos_id, a.crckey from secs as a where a.secInfos_id=-1;
+update sec_infos as b left join secs as a on b.secs_id=a.id set a.secInfos_id=b.id where a.secInfos_id=-1;
