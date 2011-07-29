26e1f83ef7386a557e53f8e3a7ba29029ceb328a
Guibin Zhang <guibin.beijing@gmail.com>
2011-07-29 13:29:32 +0800

    Rename private members of MarketDepth

--- libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/model/MarketDepth.scala
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/model/MarketDepth.scala
@@ -22,42 +22,42 @@ object MarketDepth {
  * 3 - ask size
  */
 @serializable @cloneable
-final class MarketDepth(_bidAsks: Array[Double], _bidOrders : ArrayList[Double], _askOrders : ArrayList[Double]) {
+final class MarketDepth($bidAsks: Array[Double], $bidOrders : ArrayList[Double], $askOrders : ArrayList[Double]) {
   @transient var isChanged: Boolean = _
 
   def this(_bidAsks: Array[Double]) = this(_bidAsks, null, null)
   def this() = this(null, null, null)
 
-  def bidOrders = _bidOrders
+  def bidOrders = $bidOrders
   def bidOrders_=(that: ArrayList[Double]) {
     for(bid <- that) {
-      _bidOrders += bid
+      $bidOrders += bid
     }
   }
 
-  def askOrders = _askOrders
+  def askOrders = $askOrders
   def askOrders_=(that: ArrayList[Double]) {
     for(ask <- that) {
-      _askOrders += ask
+      $askOrders += ask
     }
   }
 
-  def depth = _bidAsks.length / 4
+  def depth = $bidAsks.length / 4
   
-  def bidPrice(idx: Int) = _bidAsks(idx * 4)
-  def bidSize (idx: Int) = _bidAsks(idx * 4 + 1)
-  def askPrice(idx: Int) = _bidAsks(idx * 4 + 2)
-  def askSize (idx: Int) = _bidAsks(idx * 4 + 3)
+  def bidPrice(idx: Int) = $bidAsks(idx * 4)
+  def bidSize (idx: Int) = $bidAsks(idx * 4 + 1)
+  def askPrice(idx: Int) = $bidAsks(idx * 4 + 2)
+  def askSize (idx: Int) = $bidAsks(idx * 4 + 3)
 
   def setBidPrice(idx: Int, v: Double) = updateDepthValue(idx * 4, v)
   def setBidSize (idx: Int, v: Double) = updateDepthValue(idx * 4 + 1, v)
   def setAskPrice(idx: Int, v: Double) = updateDepthValue(idx * 4 + 2, v)
   def setAskSize (idx: Int, v: Double) = updateDepthValue(idx * 4 + 3, v)
 
-  def bidAsks = _bidAsks
+  def bidAsks = $bidAsks
   def bidAsks_=(that: Array[Double]) {
     isChanged = false
-    if (that.length != _bidAsks.length) {
+    if (that.length != $bidAsks.length) {
       return
     }
     var i = 0
@@ -69,10 +69,10 @@ final class MarketDepth(_bidAsks: Array[Double], _bidOrders : ArrayList[Double],
   }
 
   private def updateDepthValue(idx: Int, v: Double) {
-    if (_bidAsks(idx) != v) {
+    if ($bidAsks(idx) != v) {
       isChanged = true
     }
-    _bidAsks(idx) = v
+    $bidAsks(idx) = v
   }
 
 }
