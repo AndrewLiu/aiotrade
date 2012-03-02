81f1e3063ef6bcb538ce695ab3b0ad326ecc6d7b
Huang Yingjin <huangyingjin@foundersc.com>
2012-03-02 09:03:46 +0800

    The Sector can not be serialized, make a LightSector to secrialize and transfer. Signed-off-by:Huang Yingjin <huangyingjin@foundersc.com>

---
+++ libs/lib.securities/src/main/scala/org/aiotrade/lib/securities/model/LightSector.scala
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.aiotrade.lib.securities.model
+
+@serializable
+class LightSector extends CRCLongId{
+
+    var category: String = ""
+    var code: String = ""
+    var name: String = ""
+    var childrenString: String = ""
+    var parent: LightSector = _
+    lazy val key = Sector.toKey(category, code)
+
+    def copyFrom(another: LightSector){
+        this.code = another.code
+        this.name = another.name
+        this.crckey = another.crckey
+        this.childrenString = another.childrenString
+    }
+
+}
