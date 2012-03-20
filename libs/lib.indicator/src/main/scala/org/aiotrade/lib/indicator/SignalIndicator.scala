b393d71d840716adff35550a2e5b3efa723c1dd0
Caoyuan Deng <dcaoyuan@gmail.com>
2012-03-21 03:49:01 +0800

    Fixed: java.lang.StackOverflowError

--- libs/lib.indicator/src/main/scala/org/aiotrade/lib/indicator/SignalIndicator.scala
+++ libs/lib.indicator/src/main/scala/org/aiotrade/lib/indicator/SignalIndicator.scala
@@ -114,11 +114,11 @@ abstract class SignalIndicator($baseSer: BaseTSer) extends Indicator($baseSer) w
   }
 
   protected def sign(idx: Int, side: Side): Sign = {
-    sign(idx, side)
+    sign(idx, side, 0, null, null)
   }
 
   protected def sign(idx: Int, side: Side, id: Int): Sign = {
-    sign(idx, side, id)
+    sign(idx, side, id, null, null)
   }
 
   protected def sign(idx: Int, side: Side, color: Color): Sign = {
